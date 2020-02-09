edaf2f277e5003bb6462868ca2ad88c2133be8f9
diegoss3d <diegoss3d@gmail.com>
2020-02-09 02:26:29 -0300

    Peguei as questoes e a resposta certa

--- script_questionario.js
+++ script_questionario.js
@@ -1,21 +1,69 @@
 /*PEGAR QUESIONÁRIO JSON
 -----------------------------------------------------------------------*/
 var corpo = document.getElementsByName('body')
-var btnNext = document.querySelector('#next')
+/*var btnNext = document.querySelector('#next')*/
 var pergunta = document.querySelector('form #txtPergunta')
+var pos = 1;
+console.log(pos)
+
+function aumentaPos(proximaQuestao){
+    pos += 1
+}
 
 
 
 
-function loadQuestion(win, doc){
+
+
+function loadQuestion(){
     fetch('questoes.json').then(function(response) {
         return response.json();
-    }).then(function (obj) {
-        console.log(obj[1]["enunciado"])
-        pergunta.innerHTML = `${obj[4]["enunciado"]}`
-    });
+    }).then(function (obj, proximaQuestao) {
+        /*var pos = 1;*/
+        var numPergunta = 1
+        console.log(obj[1]["enunciado"]);
+        pergunta.innerHTML = `${obj[pos]["enunciado"]}`
 
-    
+        
+        var pontos = 0;
+
+        var abc = document.getElementsByName('option');
+        var escolha = ''
+
+        if(abc[0].checked) {
+            escolha == 'a'
+        }else if(abc[1].checked) {
+            escolha == 'b'
+        }else if(abc[2].checked) {
+            escolha == 'c'
+        }else if(abc[3].checked) {
+            escolha =='d'
+        }else{
+            escolha == 'e'
+        };
+
+        console.log(obj[numPergunta]["correct"]);
+
+        var respostaCerta = obj[pos]["correct"];
+
+        if(escolha == respostaCerta){
+            pontos ++;
+        };
+        
+
+
+    });  
 
 };
 
+
+
+var btnNext = document.querySelector('button#next').addEventListener('click', proximaQuestao)
+        
+
+        function proximaQuestao(pos){
+            pos += 1;
+            alert('Foi')
+        };
+        
+        proximaQuestao(aumentaPos())
--- teste.js
+++ teste.js
@@ -1,6 +1,5 @@
-"use strict";
-
-var dados = fetch("https://raw.githubusercontent.com/diegoss3d/prova/master/teste.json")
+/*PEGANDO JSON DA REDE*/
+/*var dados = fetch("https://raw.githubusercontent.com/diegoss3d/prova/master/teste.json")
 
     .then(function(resp){
         return resp.json();
@@ -8,3 +7,32 @@ var dados = fetch("https://raw.githubusercontent.com/diegoss3d/prova/master/test
     .then(function(data){
         console.log(data[0])
     });
+
+
+
+    /*PEGANDO JSON LOCAL*/
+    /*fetch('questoes.json').then(function(response) {
+        return response.json();
+    }).then(function (obj) {
+        console.log(obj[1]["enunciado"])
+    });*/
+
+
+
+
+
+
+
+/*var corpo = document.getElementsByName('body')
+var btnNext = document.querySelector('#next')
+var pergunta = document.querySelector('form #txtPergunta')*/
+
+
+
+
+
+/*function loadQuestion(jsonObj){
+    questoes = document.write = data[2]["enunciado"]
+};
+
+<p><input type="radio" name="option" id="option" value=""> e)</p>*/
