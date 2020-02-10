84a862b29b5a1b66bae88c5ed2546e4758172ebd
diegoss3d <diegoss3d@gmail.com>
2020-02-10 01:33:57 -0300

    consegui corrigir a sequencia das questoes

--- script_questionario.js
+++ script_questionario.js
@@ -2,49 +2,30 @@ var corpo = document.getElementsByName('body')
 var btnNext = document.querySelector('#next')
 var pergunta = document.querySelector('form #txtPergunta')
 var ilustra = document.querySelector('div#ilustracao')
-var pos = 1;
-console.log(next())
+var numBola = document.querySelector('#bola')
+var abc = document.getElementsByName('option');
+var pos = 0;
 
 btnNext.addEventListener('click', next);
+pergunta.innerHTML = 'Boa sorte, clique avançar para começar'
 
 function next(){
+
     pos++
+    numBola.innerHTML = pos
     console.log(pos)
-    return pos
     
-}next();
-//loadQuestion(pos);
-//respostaUser(pos);
-//renderAlternatives();
 
-
-
-/*
-LER O ENUNCIADO
- */
-function loadQuestion(){
+    /*LER O ENUNCIADO*/
     fetch('questoes.json').then(function(response) {
         return response.json();
-    }).then(function (txtEnunciado) {
-        next(pos)
-        console.log(txtEnunciado[pos]["enunciado"]);        
-        pergunta.innerHTML = `${txtEnunciado[pos]["enunciado"]}`
-        pergunta = ''
-    }); next(pos)
-};
+    }).then(function (dados) {        
+        console.log(dados[pos]["enunciado"]);        
+        pergunta.innerHTML = dados[pos]["enunciado"]
+         
 
-
-
-/*
-VERIFICAR RESPOSTA DO USUÁRIO
- */
-function respostaUser(){
-    fetch('questoes.json').then(function(response) {
-        return response.json();
-    }).then(function (txtResposta) {
-        //var pos = 1
-        console.log(txtResposta[1]["correct"]);
-        var abc = document.getElementsByName('option');
+    
+    
         var escolha = ''
 
         if(abc[0].checked) {
@@ -59,56 +40,32 @@ function respostaUser(){
             escolha == 'e'
         };
         
-        var respostaCerta = txtResposta[1]["correct"];
+        var respostaCerta = dados[pos]["correct"]
+        console.log(dados[pos]["correct"])
 
         if(escolha == respostaCerta){
             pontos += 1;
         };
-         next(pos)       
-    }); 
-}//respostaUser()
 
 
 
-/*EXIBE ALTERNATIVAS*/
-var alternativa = document.getElementsByTagName('label')
-
-function renderAlternatives(){
-    fetch('questoes.json').then(function(response) {
-        return response.json();
-    }).then(function (txtAlternativa) {        
-        alternativa[0].innerHTML += txtAlternativa[pos]["alternativas"]["a"];
-        alternativa[1].innerHTML += txtAlternativa[pos]["alternativas"]["b"];
-        alternativa[2].innerHTML += txtAlternativa[pos]["alternativas"]["c"];
-        alternativa[3].innerHTML += txtAlternativa[pos]["alternativas"]["d"];
-        alternativa[4].innerHTML += txtAlternativa[pos]["alternativas"]["e"]; 
-        next(pos)       
-    });
-}//renderAlternatives()
+    /*EXIBE ALTERNATIVAS*/
+    var alternativa = document.getElementsByTagName('label')          
+        alternativa[0].innerHTML = dados[pos]["alternativas"]["a"];
+        alternativa[1].innerHTML = dados[pos]["alternativas"]["b"];
+        alternativa[2].innerHTML = dados[pos]["alternativas"]["c"];
+        alternativa[3].innerHTML = dados[pos]["alternativas"]["d"];
+        alternativa[4].innerHTML = dados[pos]["alternativas"]["e"]; 
+             
+   
 
 
-function renderImage(){    
-    var ilustra = document.querySelector('img#ilustracao')
-    ilustra.setAttribute('id', 'foto')
-    if(pos == 1){        
-        document.querySelector('img#foto').src="./imagens/1.png";     
-    }else if(pos == 2){
-        document.querySelector('img#foto').src="./imagens/2.png";
-    }else if(pos == 3){
-        document.querySelector('img#foto').src="./imagens/3.png";
-    }else if(pos == 4){
-        document.querySelector('img#foto').src="./imagens/4.png";
-    }else if(pos == 5){
-        document.querySelector('img#foto').src="./imagens/5.png";
-    }else if(pos == 6){
-        document.querySelector('img#foto').src="./imagens/6.png";
-    }else if(pos == 7){
-        document.querySelector('img#foto').src="./imagens/7.png";
-    }else if(pos == 8){
-        document.querySelector('img#foto').src="./imagens/8.png";
-    }else if(pos == 9){
-        document.querySelector('img#foto').src="./imagens/9.png";
-    }else if(pos == 10){
-        document.querySelector('img#foto').src="./imagens/10.png";
-    }
+    /*EXIBE FOTOS*/
+   
+    
+    
+    return pos
+});
+    
 };
+
